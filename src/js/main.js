ad0a2c5ecf0d4a932444f1d04c5b963dde013b2b
canosaer <canosaer@gmail.com>
2020-12-15 15:48:13 -0500

    fixed expanded nav

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,6 @@ let hamburderDIV = document.querySelector(`.menu-toggle`)
 let expanedDIV = document.querySelector(`.expanded`)
 let expandedStyles = window.getComputedStyle(expandedDIV)
 
-console.log(`yo`)
-
 hamburgerDIV.addEventListener(`click`, function(){
     if(expandedStyles.getPropertyValue(`display`) === `none`) {expandedDIV.style.display = `flex`}
     else expandedDIV.style.display = `none`
